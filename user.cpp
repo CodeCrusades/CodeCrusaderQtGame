789be82ef794cad19c0a63e3899b174b147e09f6
Lauryn Claire Hansen <lauryn.c.hansen@gmail.com>
2023-04-24 11:23:08 -0600

    Pushing user updates.

--- user.cpp
+++ user.cpp
@@ -257,3 +257,4 @@ QDate user::getTodayDate() {
 
 
 
+}
--- user.h
+++ user.h
@@ -8,7 +8,6 @@ class user : public QObject{
 //    Q_OBJECT
 public:
     QString birthDay;
-    //int *birthDay;
     QString password;
     QString username;
     QString firstName;
@@ -22,14 +21,14 @@ public:
     bool login();
     bool verifyPassword();
     bool hasBirthdayToday();
-    QString username;
+    //QString username;
     int dateOfBirth;
     QImage profilePicture;
 
     user(QString passwordIn, QString firstNameIn, QString lastNameIn,  QString usernameIn, int dateOfBirthIn);
-    bool login();
-    bool verifyPassword();
-    bool hasBirthdayToday();
+    //bool login();
+    //bool verifyPassword();
+   // bool hasBirthdayToday();
     bool signup(int birthDay, QString username, QString password, QString firstName, QString lastName, parser parser);
     bool validPassword(QString password);
     QDate getTodayDate();
