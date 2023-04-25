b246b8359f15ddb2d7643d6b5c49db85b8b2bf70
LeviNeely <levijneely@gmail.com>
2023-04-24 20:55:19 -0600

    Updated images, sounds, and cleaned up code, also updated a few .h files

--- game1scene.cpp
+++ game1scene.cpp
@@ -19,69 +19,63 @@ game1scene::game1scene(int level)
     : QGraphicsScene()
 {
     this->level = level;
-   // allDrops = new QVector<waterDroplet*>();
-  // add bucket
-  bucketItem = new bucket();
-  addItem(bucketItem);
-  bucketItem->setPixmap(QPixmap(":/images/bucket.png").scaled(150, 150));
-  bucketItem->setFlag(QGraphicsItem::ItemIsFocusable);
-  bucketItem->setFocus();
-  bucketItem->setPos(400, 365);
+    bucketItem = new bucket();
+    addItem(bucketItem);
+    bucketItem->setFlag(QGraphicsItem::ItemIsFocusable);
+    bucketItem->setFocus();
+    bucketItem->setPos(400, 400);
 
-  // add background
-  //Change background to be a gif??????????
-  setBackgroundBrush(QBrush(QImage(":/images/background.jpg").scaledToHeight(512) .scaledToWidth(910)));
-  setSceneRect(0, 0, 908, 510);
+    // add background
+    setBackgroundBrush(QBrush(QImage(":/images/8bit.jpg").scaledToHeight(512) .scaledToWidth(910)));
+    setSceneRect(0, 0, 908, 510);
 
-  // add the cloud
-  cloud = new QGraphicsPixmapItem(QPixmap("://images/cloud.png").scaled(380,90));
-  cloud->setPos(250, 0);
-  addItem(cloud);
+    // add the cloud
+    cloud = new QGraphicsPixmapItem(QPixmap("://images/cloud.png").scaled(380,90));
+    cloud->setPos(250, 0);
+    addItem(cloud);
 
     //waterDropletCounter = 0;
-  points = new int(0);
-  collectedWaterDroplets = new int(0);
-  missedWaterDroplets = new int(0);
-  missedFiveDroplet = false;
+    points = new int(0);
+    collectedWaterDroplets = new int(0);
+    missedWaterDroplets = new int(0);
+    missedFiveDroplet = false;
 
-  win = false;
+    win = false;
 
-  // add multiple droplets to game scence
-  spawnDropletsTimer = new QTimer(this);
-  connect(spawnDropletsTimer, &QTimer::timeout, this, &game1scene::generateDropletAndCount);
-  // Create the level selection menu
-  setGameLevel(level);
-  //Figure out a way to stop drops from spawning, UPDATE: JUST USE THE ->STOP() METHOD OF THE TIMER LOL
+    // add multiple droplets to game scence
+    spawnDropletsTimer = new QTimer(this);
+    connect(spawnDropletsTimer, &QTimer::timeout, this, &game1scene::generateDropletAndCount);
+    // Create the level
+    setGameLevel(level);
 
-  // slot to move the cloud
-  flagToMoveCloudHorizontal = false;
-  QTimer *cloudTimer = new QTimer(this);
-  connect (cloudTimer, &QTimer::timeout, this, &game1scene::moveTheCloud);
-  cloudTimer->start(1000);
+    // slot to move the cloud
+    flagToMoveCloudHorizontal = false;
+    QTimer *cloudTimer = new QTimer(this);
+    connect (cloudTimer, &QTimer::timeout, this, &game1scene::moveTheCloud);
+    cloudTimer->start(100);
 
-  //Add background music
-  QMediaPlayer *player = new QMediaPlayer();
-  QAudioOutput *output = new QAudioOutput();
-  player->setAudioOutput(output);
-  player->setSource(QUrl("qrc:/sounds/8Bit Platformer Loop.wav"));
-  output->setVolume(50);
-  player->play();
+    //Add background music
+    player = new QMediaPlayer();
+    output = new QAudioOutput();
+    player->setAudioOutput(output);
+    player->setSource(QUrl("qrc:/sounds/8Bit Platformer Loop.wav"));
+    output->setVolume(50);
+    player->play();
 
 }
 
 
 //Method to generate a water droplet
 void game1scene::generateDropletAndCount() {
-  if (*points >= 15) {
-      win = true;
-      spawnDropletsTimer->stop();
-      removeItem(bucketItem);
-  }
+    if (*points >= 15) {
+        win = true;
+        spawnDropletsTimer->stop();
+        removeItem(bucketItem);
+    }
     if (*missedWaterDroplets >= 5) {
         missedFiveDroplet = true;
         spawnDropletsTimer->stop();
     }
-    qDebug() << "Points: " << *points << "\n" << "collectedWaterDroplets: " << *collectedWaterDroplets << "\n" << "missedWaterDroplets: " << *missedWaterDroplets << "\n";
     waterDroplet *dropleeeet = new waterDroplet(points, collectedWaterDroplets, missedWaterDroplets);
 
     int random_number = arc4random() % 700;
@@ -112,6 +106,8 @@ bool game1scene::displayWinMessage() {
 
      winWidget->setLayout(layout);
      this->addWidget(winWidget);
+     player->setSource(QUrl("qrc:/sounds/win.wav"));
+     player->play();
      removeItem(bucketItem);
 
      return true;
@@ -129,6 +125,8 @@ bool game1scene::displayLoseMessage() {
      layoutL->addWidget(closeButtonL);
      loseWidget->setLayout(layoutL);
      this->addWidget(loseWidget);
+     player->setSource(QUrl("qrc:/sounds/endgame.wav"));
+     player->play();
      removeItem(bucketItem);
      return true;
 }
@@ -150,18 +148,15 @@ void game1scene::setGameLevel(int level) {
 
 void game1scene:: game1scene::moveTheCloud() {
      // Move the cloud horizontally
-     int cloudSpeed = 40; // adjust this value to change the speed of the cloud
+     int cloudSpeed = 1; // adjust this value to change the speed of the cloud
      if(cloud->x() >= 600) flagToMoveCloudHorizontal = false;
      if(cloud->x() < 0 ) flagToMoveCloudHorizontal = true;
 
      if (!flagToMoveCloudHorizontal) {
       //left
       cloud->setPos(cloud->x() - cloudSpeed, cloud->y());
-      qInfo() << "cloud moving to the left";
      } else {
       //right
       cloud->setPos(cloud->x() + cloudSpeed, cloud->y()); // update the position of the cloud sprite
-      qInfo() << "cloud moving to the right";
-
      }
 }
--- game1scene.h
+++ game1scene.h
@@ -20,12 +20,12 @@ private:
   bool missedFiveDroplet;
   bool flagToMoveCloudHorizontal;
   QGraphicsPixmapItem *cloud;
+  QMediaPlayer *player;
+  QAudioOutput *output;
   int level;
-  //QVector<waterDroplet*> *allDrops;
 
 public:
     game1scene(int level);
-    //int waterDropletCounter;
     int *points;
     int *collectedWaterDroplets;
     int *missedWaterDroplets;
@@ -33,10 +33,9 @@ public:
 
 public slots:
     void generateDropletAndCount();
-//    void handleCollision();
     bool displayWinMessage();
     bool displayLoseMessage();
-    void setGameLevel(int level); // set the level of the game
+    void setGameLevel(int level);
     void moveTheCloud();
 };
 
